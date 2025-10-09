4425e5a11f1aac27f1046dd76ac877e1bb843e64
EslamSaeed570 <solomelprens12345@gmail.com>
2025-10-09 05:45:50 +0300

    add images and use Flutter assets Gen to generate app images

---
+++ lib/utils/app_images.dart
@@ -0,0 +1,37 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  /// Assets for imagesCardReceive
+  /// assets/Images/card-receive.svg
+  static const String imagesCardReceive = "assets/Images/card-receive.svg";
+
+  /// Assets for imagesChart2
+  /// assets/Images/chart-2.svg
+  static const String imagesChart2 = "assets/Images/chart-2.svg";
+
+  /// Assets for imagesConvertCard
+  /// assets/Images/convert-card.svg
+  static const String imagesConvertCard = "assets/Images/convert-card.svg";
+
+  /// Assets for imagesFrame
+  /// assets/Images/Frame.svg
+  static const String imagesFrame = "assets/Images/Frame.svg";
+
+  /// Assets for imagesFrame (1)
+  /// assets/Images/Frame (1).svg
+  static const String imagesFrame1  = "assets/Images/Frame (1).svg";
+
+  /// Assets for imagesFrame (2)
+  /// assets/Images/Frame (2).svg
+  static const String imagesFrame2  = "assets/Images/Frame (2).svg";
+
+  /// Assets for imagesFrame (3)
+  /// assets/Images/Frame (3).svg
+  static const String imagesFrame3  = "assets/Images/Frame (3).svg";
+
+  /// Assets for imagesGraph
+  /// assets/Images/graph.svg
+  static const String imagesGraph = "assets/Images/graph.svg";
+}
+

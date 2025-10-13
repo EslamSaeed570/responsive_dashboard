7bb2e50d5aa65dbd5bb856b9ac32d6cbe391cd91
EslamSaeed570 <solomelprens12345@gmail.com>
2025-10-13 18:20:37 +0300

    refactor custom container

--- lib/views/all_expensess.dart
+++ lib/views/all_expensess.dart
@@ -1,26 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dashboard/models/all_expensess_item_model.dart';
-import 'package:responsive_dashboard/utils/app_images.dart';
 
-import 'package:responsive_dashboard/views/widgets/all_expensess_item.dart';
 import 'package:responsive_dashboard/views/widgets/all_expensess_item_list_view.dart';
 
 import 'package:responsive_dashboard/views/widgets/all_expensive_headrs.dart';
+import 'package:responsive_dashboard/views/widgets/custom_background_container.dart';
 
 class AllExpensess extends StatelessWidget {
   const AllExpensess({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(20),
-      decoration: ShapeDecoration(
-        color: Colors.white,
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(12),
-        ),
-      ),
-      child: const Column(
+    return const CustomBackgroundContainer(
+      child: Column(
         children: [
           AllExpensessHeaders(),
           SizedBox(

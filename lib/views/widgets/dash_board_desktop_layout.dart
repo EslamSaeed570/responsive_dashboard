97073ca1801a9ff78ea2c9adcba6e15892e4623e
EslamSaeed570 <solomelprens12345@gmail.com>
2025-10-31 14:22:49 +0200

    create transaction history headers

--- lib/views/widgets/dash_board_desktop_layout.dart
+++ lib/views/widgets/dash_board_desktop_layout.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:responsive_dashboard/utils/app_styles.dart';
 
 import 'package:responsive_dashboard/views/widgets/QuickInvoiceSection/all_expenses_and_quick_invoice_section.dart';
 
 import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 
 import 'package:responsive_dashboard/views/widgets/my_card_section.dart';
+import 'package:responsive_dashboard/views/widgets/transaction_history_headers.dart';
 
 class DashBoardDesktopLayout extends StatelessWidget {
   const DashBoardDesktopLayout({super.key});
@@ -27,7 +29,28 @@ class DashBoardDesktopLayout extends StatelessWidget {
         SizedBox(
           width: 24,
         ),
-        Expanded(child: MycardSection())
+        Expanded(child: TransactionHistory())
+      ],
+    );
+  }
+}
+
+class TransactionHistory extends StatelessWidget {
+  const TransactionHistory({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        TransactionHistoryHeaders(),
+        SizedBox(
+          height: 20,
+        ),
+        Text(
+          '13 April 2022',
+          style: AppStyles.styleMedium16,
+        ),
       ],
     );
   }

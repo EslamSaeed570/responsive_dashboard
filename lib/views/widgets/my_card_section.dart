73bf02edf078b645ed6946918e939e15a864e256
EslamSaeed570 <solomelprens12345@gmail.com>
2025-10-31 07:47:41 +0200

    finish my card section

--- lib/views/widgets/my_card_section.dart
+++ lib/views/widgets/my_card_section.dart
@@ -1,29 +1,52 @@
-
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/utils/app_styles.dart';
 import 'package:responsive_dashboard/views/widgets/dots_indicator.dart';
 import 'package:responsive_dashboard/views/widgets/my_card_page_view.dart';
 
-class MycardSection extends StatelessWidget {
+class MycardSection extends StatefulWidget {
   const MycardSection({super.key});
 
   @override
+  State<MycardSection> createState() => _MycardSectionState();
+}
+
+class _MycardSectionState extends State<MycardSection> {
+  late PageController pageController;
+  int currentPageIndex = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+    pageController.addListener(
+      () {
+        currentPageIndex = pageController.page!.round();
+        setState(() {});
+      },
+    );
+
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Column(
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(
+        const Text(
           'My Card',
           style: AppStyles.styleSemiBold20,
         ),
-        SizedBox(
+        const SizedBox(
           height: 20,
         ),
-        MyCardPageView(),
-        SizedBox(
+        MyCardPageView(
+          pageController: pageController,
+        ),
+        const SizedBox(
           height: 19,
         ),
-        DotsIndicator(),
+        DotsIndicator(
+          currentPageIndex: currentPageIndex,
+        ),
       ],
     );
   }

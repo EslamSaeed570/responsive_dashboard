a3c87749d9bb4ccd508d3480c9d7ff4de228666b
EslamSaeed570 <solomelprens12345@gmail.com>
2025-11-02 15:58:55 +0200

    simple refactor

---
+++ lib/views/widgets/income_chart.dart
@@ -0,0 +1,52 @@
+
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class IncomeChart extends StatefulWidget {
+  const IncomeChart({super.key});
+
+  @override
+  State<IncomeChart> createState() => _IncomeChartState();
+}
+
+class _IncomeChartState extends State<IncomeChart> {
+  int currentIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return PieChart(getPieChartData());
+  }
+
+  PieChartData getPieChartData() {
+    return PieChartData(
+        sectionsSpace: 0,
+        pieTouchData: PieTouchData(
+          touchCallback: (p0, p1) {
+            currentIndex = p1?.touchedSection?.touchedSectionIndex ?? -1;
+            setState(() {});
+          },
+        ),
+        sections: [
+          PieChartSectionData(
+              showTitle: false,
+              value: 40,
+              color: const Color(0xff208CC8),
+              radius: currentIndex == 0 ? 50 : 40),
+          PieChartSectionData(
+              showTitle: false,
+              value: 25,
+              color: const Color(0xff4EB7F2),
+              radius: currentIndex == 1 ? 50 : 40),
+          PieChartSectionData(
+              showTitle: false,
+              value: 20,
+              color: const Color(0xff064061),
+              radius: currentIndex == 2 ? 50 : 40),
+          PieChartSectionData(
+              showTitle: false,
+              value: 22,
+              color: const Color(0xffE2DECD),
+              radius: currentIndex == 3 ? 50 : 40),
+        ]);
+  }
+}

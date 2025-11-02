45b862867b9a9915f2ba76f4d9cf42c47feb1992
EslamSaeed570 <solomelprens12345@gmail.com>
2025-11-02 12:59:34 +0200

    income chart part one

--- lib/views/widgets/income_section.dart
+++ lib/views/widgets/income_section.dart
@@ -1,4 +1,4 @@
-
+import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/income_section_headers.dart';
 
@@ -10,7 +10,32 @@ class IncomeSection extends StatelessWidget {
     return const Column(
       children: [
         IncomeSectionHeaders(),
+        Expanded(
+          child: Row(
+            children: [Expanded(child: IncomeChart(),),],
+          ),
+        )
       ],
     );
+  }
+}
+
+class IncomeChart extends StatelessWidget {
+  const IncomeChart({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PieChart(getPieChartData());
+  }
+
+  PieChartData getPieChartData() {
+    return PieChartData(
+      sectionsSpace: 0,
+      sections: [
+      PieChartSectionData(value: 20, color: Colors.red),
+      PieChartSectionData(value: 20, color: Colors.green),
+      PieChartSectionData(value: 30, color: Colors.amber),
+      PieChartSectionData(value: 40, color: Colors.blue),
+    ]);
   }
 }

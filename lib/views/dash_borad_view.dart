0f120c19b9102df3355507ea1e1dd57f3d01b532
EslamSaeed570 <solomelprens12345@gmail.com>
2025-11-05 05:24:44 +0200

    finish mobile layout

--- lib/views/dash_borad_view.dart
+++ lib/views/dash_borad_view.dart
@@ -1,19 +1,35 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dashboard/views/widgets/adaptive_layout_widget.dart';
+import 'package:responsive_dashboard/views/widgets/custom_drawer.dart';
 import 'package:responsive_dashboard/views/widgets/dash_board_desktop_layout.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_mobile_layout.dart';
 import 'package:responsive_dashboard/views/widgets/dashboard_tablet_layout.dart';
 
-class DashBoradView extends StatelessWidget {
+class DashBoradView extends StatefulWidget {
   const DashBoradView({super.key});
+
+  @override
+  State<DashBoradView> createState() => _DashBoradViewState();
+}
+
+class _DashBoradViewState extends State<DashBoradView> {
+  GlobalKey<ScaffoldState> scaffoldKey = GlobalKey();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffoldKey,
+      drawer: const Drawer(child: CustomDrawer()),
       appBar: MediaQuery.of(context).size.width < 650
           ? AppBar(
               backgroundColor: const Color(0xffFAFAFA),
-              leading: const Icon(Icons.menu),
+              leading: IconButton(
+                onPressed: () {
+                  scaffoldKey.currentState!.openDrawer();
+                  setState(() {});
+                },
+                icon: const Icon(Icons.menu),
+              ),
             )
           : null,
       backgroundColor: const Color(0xffF7F9FA),
--- lib/views/widgets/dashboard_mobile_layout.dart
+++ lib/views/widgets/dashboard_mobile_layout.dart
@@ -23,7 +23,7 @@ class DashboardMobileLayout extends StatelessWidget {
           ),
           IncomeSection(),
           SizedBox(
-            height: 20,
+            height: 40,
           ),
         ],
       ),
